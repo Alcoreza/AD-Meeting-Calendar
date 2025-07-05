9c16fab861d8de0bad96cec3a4f74bc1e2e20996
Alcoreza <202310642@fit.edu.ph>
2025-07-05 17:56:37 +0800

    fix(bootstrap.php): fixed the path codes for condition of redefinement

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,13 @@
 <?php
-define('BASE_PATH', realpath(__DIR__));
-define('UTILS_PATH', BASE_PATH . '/utils/');
-define('VENDOR_PATH', BASE_PATH . '/vendor/');
-define('HANDLERS_PATH', BASE_PATH . '/handlers/');
-define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
-
+if (!defined('BASE_PATH'))
+    define('BASE_PATH', realpath(__DIR__));
+if (!defined('UTILS_PATH'))
+    define('UTILS_PATH', BASE_PATH . '/utils/');
+if (!defined('VENDOR_PATH'))
+    define('VENDOR_PATH', BASE_PATH . '/vendor/');
+if (!defined('HANDLERS_PATH'))
+    define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+if (!defined('DUMMIES_PATH'))
+    define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
 chdir(BASE_PATH);
+
