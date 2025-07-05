af8bd8b07e976c90f49598f4a6a4abf6962282a7
Alcoreza <202310642@fit.edu.ph>
2025-07-05 18:09:42 +0800

    feat: added the code for calling the migrate file

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 include HANDLERS_PATH . 'mongodbChecker.handler.php';
 include HANDLERS_PATH . 'postgreChecker.handler.php';
 include UTILS_PATH . 'dbSeederPostgresql.util.php';
+include UTILS_PATH . 'dbMigratePostgresql.util.php';
 ?>
