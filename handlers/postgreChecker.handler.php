82f6e11cc35d7420e2f16226522974d2059aefaf
Alcoreza <202310642@fit.edu.ph>
2025-07-15 13:48:54 +0800

    fix: fixed the reading of names of the tables

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -2,20 +2,21 @@
 
 require_once UTILS_PATH . 'envSetter.util.php';
 
-$host = "host.docker.internal";
-$port = "5112";
-$username = "user";
-$password = "password";
-$dbname = "mydatabase";
+$host = $_ENV['PG_HOST'] ?? null;
+$port = $_ENV['PG_PORT'] ?? null;
+$username = $_ENV['PG_USER'] ?? null;
+$password = $_ENV['PG_PASS'] ?? null;
+$dbname = $_ENV['PG_DB'] ?? null;
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
 $dbconn = pg_connect($conn_string);
 
 if (!$dbconn) {
-    echo "❌ Connection Failed: ", pg_last_error() . "  <br>";
-    exit();
+    $pgStatus = "❌ Connection Failed: ";
 } else {
     $pgStatus = "✅ Connected to PostgreSQL successfully.";
     pg_close($dbconn);
 }
+
+return $pgStatus;
